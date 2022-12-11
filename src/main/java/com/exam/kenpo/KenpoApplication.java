ff5536119569ef0e54e11684b91f9c02309fa68f
jesusclever307 <jlinares@cleveritgroup.com>
2022-12-10 23:23:12 -0300

    first commit

---
+++ src/main/java/com/exam/kenpo/KenpoApplication.java
@@ -0,0 +1,13 @@
+package com.exam.kenpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/kenpo/KenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.kenpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
